8678d987dc2b414525d772212100735e5ee306d6
auwaldev-creator <auwalbashar12@gmail.com>
2026-02-13 12:44:46 +0100

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+  serverExternalPackages: ['pdf-lib'],
+}
+
+export default nextConfig
